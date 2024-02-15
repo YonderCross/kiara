c987677c9716dd813d263679887c00bfff0f2590
Yondercross <verasm301296@hotmail.com>
2024-02-14 21:36:36 -0600

    agrego a yonder

--- Practica1/src/Main.java
+++ Practica1/src/Main.java
@@ -6,7 +6,9 @@ public class Main {
 		System.out.println("Holi");
 		System.out.println("Kiara2");
 		System.out.println("Esta es una pprueba de git ")
-		
+		System.out.println("Esta es una pprueba de git que solo debe salir en yonder ")
+
+
 		System.out.println(sum(6,8));
 
 		System.out.println("Brave");
