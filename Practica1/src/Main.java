e2e7b49a69ebe737e0635e211a982d2d9fc32b9f
Yondercross <verasm301296@hotmail.com>
2024-02-14 21:40:06 -0600

    agrego a yonder update

--- Practica1/src/Main.java
+++ Practica1/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		System.out.println("Holi");
 		System.out.println("Kiara2");
 		System.out.println("Esta es una pprueba de git ")
-		System.out.println("Esta es una pprueba de git que solo debe salir en yonder ")
+		System.out.println("Esta es una pprueba de git que solo debe salir en yonder y esta sale solo en main  ")
 
 
 		System.out.println(sum(6,8));
